21147a7d18239f77be818c217be87e7c49909e85
roman.klymovych <roman.klymovych@avenga.com>
2021-03-09 18:55:52 +0200

    Try to bind dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import Signup from "./Signup";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from "react-bootstrap";
-import {AuthProvider} from "../context/AuthContext";
+import {AuthProvider, useAuth} from "../context/AuthContext";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import {Dashboard} from "./Dashboard";
+// import {Dashboard} from "./Dashboard";
+import Dashboard from "../DashboardLayout/index";
 import {Login} from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import {ForgotPassword} from "./ForgotPassword";
 import {UpdateProfile} from "./UpdateProfile";
+import Account from "../views/account/AccountView";
+import Profile from "../views/account/AccountView/index";
+
 
 function App() {
+  const {currentUser} = useAuth()
+  // console.log(currentUser)
   return (
 
       <Container
@@ -20,12 +26,18 @@ function App() {
         <div className="w-100" style={{maxWidth: '400px'}}>
           <Router>
             <AuthProvider>
+              {/*{currentUser && <Dashboard/>}*/}
+              {/* делать правильно роуты!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
+
               <Switch>
+                {/*<PrivateRoute exact path='/' component={Dashboard}/>*/}
                 <PrivateRoute exact path='/' component={Dashboard}/>
                 <PrivateRoute exact path='/update-profile' component={UpdateProfile}/>
+                <PrivateRoute path='/profile' component={Profile}/>
+
                 <Route path='/signup' component={Signup}/>
                 <Route path='/login' component={Login}/>
-                <Route path='/forgot-password' component={ForgotPassword}/>
+                <Route path="/profile" component={Account}/>
               </Switch>
             </AuthProvider>
           </Router>
