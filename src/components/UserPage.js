f927d0ec0eb1eb0d6d6535572334931dadf7e80b
roman.klymovych <roman.klymovych@avenga.com>
2021-03-16 18:52:27 +0200

    was added modal window for users.

---
+++ src/components/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Grid, Typography} from "@material-ui/core";
+
+
+export const UserPage =() =>{
+  return (
+      <Grid container spacing={3}>
+        <Typography variant='p'>
+          Hello UserPage
+        </Typography>
+      </Grid>
+  )
+}
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { SideNav } from "./SideNav";
-import { db } from "../firebase";
-import { useAuth } from "../context/AuthContext";
-import { Avatar, ListItem, ListItemAvatar, ListItemText, makeStyles } from "@material-ui/core";
+import React, {useEffect, useState} from 'react'
+import {SideNav} from "./SideNav";
+import {db} from "../firebase";
+import {useAuth} from "../context/AuthContext";
+import {Avatar, ListItem, ListItemAvatar, ListItemText, makeStyles} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
+import {UserModal} from "./UserModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,62 +29,78 @@ const useStyles = makeStyles((theme) => ({
 
 export const Users = () => {
   const classes = useStyles()
-  const { getUid } = useAuth()
+  const {getUid} = useAuth()
   const [users, setUsers] = useState([])
+  const [currentUser, setCurrentUser] = useState()
+  const [openModal, setOpenModal] = useState(false)
 
   const getUsers = () => {
-    return db.collection("users").get()
-      .then((querySnapshot) => {
-        const arr = querySnapshot.docs.filter((user => getUid() !== user.id)).map((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.data().uid);
-          return doc.data()
-        })
-        setUsers(arr);
-      });
+    return db.collection("users").get() // надо ли ретурн???
+        .then((querySnapshot) => {
+          const users = querySnapshot.docs.filter((user => getUid() !== user.id)).map((doc) => {
+            return {id: doc.id, ...doc.data()};
+          })
+          setUsers(users);
+        });
   }
 
   useEffect(() => {
     getUsers()
   }, [])
 
+  const handleOpenUserModal = (user) => {
+    setCurrentUser(user)
+    setOpenModal(!openModal)
+  }
+
   return (
-    <SideNav>
-      <List
-        className={classes.root}
-      >
-        {users.length && users.map((user, idx) => {
-          return (
-            <ListItem key={idx} alignItems="flex-start" className={classes.listItem}>
-              <ListItemAvatar>
-                <Avatar
-                  alt={user.avatar}
-                  src={user.avatar}
-                  onClick={() => console.log(user.uid)}
-                />
-              </ListItemAvatar>
-              <ListItemText
-                primary={user.name}
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      component="span"
-                      variant="body2"
-                      className={classes.inline}
-                      color="textPrimary"
-                    >
-                    </Typography>
-                    {user.description}
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          )
-        })}
-
-
-      </List>
-    </SideNav>
+      <>
+        <SideNav>
+          <List
+              className={classes.root}
+          >
+            {users.length && users.map(user => {
+              // if(user.isOnline){    // flag isOnline
+              return (
+                  <ListItem
+                      key={user.id}
+                      alignItems="flex-start"
+                      className={classes.listItem}
+                      onClick={() => handleOpenUserModal(user)}
+                  >
+                    <ListItemAvatar>
+                      <Avatar
+                          alt={user.avatar}
+                          src={user.avatar}
+                      />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={user.name}
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                                component="span"
+                                variant="body2"
+                                className={classes.inline}
+                                color="textPrimary"
+                            >
+                            </Typography>
+                            {user.description}
+                            {user.id}
+                          </React.Fragment>
+                        }
+                    />
+                  </ListItem>
+              )
+              // }
+            })}
+          </List>
+        </SideNav>
+        <UserModal
+            openModal={openModal}
+            setOpenModal={setOpenModal}
+            currentUser={currentUser}/>
+      </>
 
   )
 }
